353469c79ea23aa404d06e29f24bbf8b7cf6bf79
chaeni_beam <twokids1005@naver.com>
2024-11-30 22:53:07 +0900

    20241129

---
+++ app/src/main/java/com/chaeni__beam/ai_handwriting_correction_platform/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.chaeni__beam.ai_handwriting_correction_platform
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
